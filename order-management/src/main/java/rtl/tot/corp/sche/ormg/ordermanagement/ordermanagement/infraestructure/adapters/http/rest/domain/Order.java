17246f92c835a81665ceee66ffa161c46cfed565
ext_lavergarav <ext_lavergarav@MOEXT_LAVER.Falabella.com>
2019-03-03 23:16:18 -0300

    init commit

---
+++ order-management/src/main/java/rtl/tot/corp/sche/ormg/ordermanagement/ordermanagement/infraestructure/adapters/http/rest/domain/Order.java
@@ -0,0 +1,13 @@
+package rtl.tot.corp.sche.ormg.ordermanagement.ordermanagement.infraestructure.adapters.http.rest.domain;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+ 
+@Data
+public class Order {
+	@NotNull
+	String orderId;	
+
+}
+
